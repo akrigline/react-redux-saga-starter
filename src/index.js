e312423ee464207af1fc7c93a6e0b249e966df16
Andrew Krigline <andrew.krigline@robotsandpencils.com>
2017-06-22 15:58:58 -0400

    Ejected and configured css modules with scss support

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './scenes/root/rootScene'
 import registerServiceWorker from './registerServiceWorker';
-import './style/main.css';
+import './style/main.scss';
 import createReduxStore from './redux/store'
 
 
