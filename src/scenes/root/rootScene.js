0bc6852402571c6d852c833eda1ea000f509e390
Andrew Krigline <andrew.krigline@robotsandpencils.com>
2017-07-03 14:05:09 -0400

    added code splitting of views via Route with react-loadable

--- src/scenes/root/rootScene.js
+++ src/scenes/root/rootScene.js
@@ -1,26 +1,29 @@
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-// import routes from '../../routes';
-import AppWrapper from '../appWrapper/appWrapperScene';
-import HomePage from '../homePage/homePageScene';
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Loadable from 'react-loadable'
+import AppWrapper from '../appWrapper/appWrapperScene'
 
+const AsyncHomePage = Loadable({
+  loader: () => import('../homePage/homePageScene'),
+  loading: () => null
+})
 
 export default class Root extends Component {
   render() {
-    const { store } = this.props;
+    const { store } = this.props
     return (
       <Provider store={store}>
         <BrowserRouter>
           <AppWrapper>
             <Switch>
-              <Route path='/' component={HomePage} />
+              <Route path='/' exact component={AsyncHomePage} />
               
             </Switch>
           </AppWrapper>
         </BrowserRouter>
       </Provider>
-    );
+    )
   }
   props: {
     store: Object
